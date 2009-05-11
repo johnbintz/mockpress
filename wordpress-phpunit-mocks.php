8b08196d6e9943f654c39ab8d4e4022ab55b295f
John Bintz <jcoswell@coswellproductions.org>
2009-05-11 18:33:52 -0400

    initial commit

---
+++ wordpress-phpunit-mocks.php
@@ -0,0 +1,70 @@
+<?php
+
+$wp_test_expectations = array();
+
+function _reset_wp() {
+	global $wp_test_expectations;
+	$wp_test_expectations = array();
+}
+
+/* WordPress Test Doubles */
+
+function get_option($key) {
+  global $wp_test_expectations;
+  if (isset($wp_test_expectations['options'][$key])) {
+    return $wp_test_expectations['options'][$key];
+  } else {
+    return false;
+  }
+}
+                
+function update_option($key, $value) {
+  global $wp_test_expectations;
+  if ($wp_test_expectations['options'][$key] == $value) {
+    return false;
+  } else {
+    $wp_test_expectations['options'][$key] = $value;
+    return true;
+  }
+}
+                                    
+function delete_option($key) {
+  global $wp_test_expectations;
+  if (isset($wp_test_expectations['options'][$key])) {
+    unset($wp_test_expectations['options'][$key]);
+    return true;
+  } else {
+    return false;
+  }
+}
+                    
+function untrailingslashit($string) {
+  return preg_replace('#/$#', '', $string);
+}
+
+function add_category($id, $object) {
+  global $wp_test_expectations;
+  $wp_test_expectations['categories'][$id] = $object;
+}
+    
+function get_category($id) {
+  global $wp_test_expectations;
+  return $wp_test_expectations['categories'][$id];
+}
+        
+function get_all_category_ids() {
+  global $wp_test_expectations;
+  return array_keys($wp_test_expectations['categories']);
+}
+
+function get_gmt_from_date($date_string) {
+  return $date_string;
+}
+  
+function get_cat_name($id) {
+  global $wp_test_expectations;
+  return $wp_test_expectations['categories'][$id]->name;
+}
+                                        
+
+?>
