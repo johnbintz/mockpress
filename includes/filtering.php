8ee6ee7725b4424b484679a6c77fa14f32fb6bfb
John Bintz <john@coswellproductions.com>
2010-01-20 07:27:55 -0500

    add new filter string

--- includes/filtering.php
+++ includes/filtering.php
@@ -6,3 +6,4 @@ function esc_html($string) { return $string; }
 function esc_attr($string) { return $string; }
 function attribute_escape($string) { return $string; }
 function esc_js($string) { return $string; }
+function wp_filter_nohtml_kses($string) { return $string; }
