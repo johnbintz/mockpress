727934594dd686c689a50611ca95823aed36c155
John Bintz <john@coswellproductions.com>
2010-02-27 10:19:51 -0500

    new comment and get comment

--- includes/comments.php
+++ includes/comments.php
@@ -23,11 +23,46 @@ function wp_insert_comment($comment_data) {
 	if ( ! isset($comment_type) )
 		$comment_type = '';
 
-	$wp_test_expectations['comments'][] = compact(
+	$id = count($wp_test_expectations['comments']) + 1;
+
+	$wp_test_expectations['comments'][$id] = compact(
 		'comment_post_ID', 'comment_author', 'comment_author_email',
 		'comment_author_url', 'comment_author_IP', 'comment_date',
 		'comment_date_gmt', 'comment_content', 'comment_karma',
 		'comment_approved', 'comment_agent', 'comment_type',
 		'comment_parent', 'user_id'
 	);
+
+	return $id;
+}
+
+function wp_new_comment($commentdata) {
+	// taken straight from WPMU 2.9.11 source...
+	$commentdata['comment_post_ID'] = (int) $commentdata['comment_post_ID'];
+	if ( isset($commentdata['user_ID']) )
+		$commentdata['user_id'] = $commentdata['user_ID'] = (int) $commentdata['user_ID'];
+	elseif ( isset($commentdata['user_id']) )
+		$commentdata['user_id'] = (int) $commentdata['user_id'];
+
+	if (isset($_SERVER['REMOTE_ADDR'])) {
+		$commentdata['comment_author_IP'] = preg_replace('/[^0-9a-fA-F:., ]/', '', $_SERVER['REMOTE_ADDR']);
+	}
+
+	if (isset($_SERVER['HTTP_USER_AGENT'])) {
+		$commentdata['comment_agent'] = substr($_SERVER['HTTP_USER_AGENT'], 0, 254);
+	}
+
+	return wp_insert_comment($commentdata);
+}
+
+function get_comment($comment) {
+	global $wp_test_expectations;
+
+	if (is_numeric($comment)) {
+		if (isset($wp_test_expectations['comments'][$comment])) {
+			return $wp_test_expectations['comments'][$comment];
+		} else {
+			return false;
+		}
+	}
 }
--- test/CommentsTest.php
+++ test/CommentsTest.php
@@ -6,6 +6,7 @@ require_once(dirname(__FILE__) . '/../includes/comments.php');
 class CommentsTest extends PHPUnit_Framework_TestCase {
 	function setUp() {
 		_reset_wp();
+		$_SERVER = array();
 	}
 
 	function providerTestInsertComment() {
@@ -68,10 +69,11 @@ class CommentsTest extends PHPUnit_Framework_TestCase {
 	function testInsertComment($comment_data, $expected_comment) {
 		global $wp_test_expectations;
 
-		wp_insert_comment($comment_data);
+		$id = wp_insert_comment($comment_data);
 
+		$this->assertEquals(1, $id);
 		$this->assertEquals(1, count($wp_test_expectations['comments']));
-		$comment = $wp_test_expectations['comments'][0];
+		$comment = get_comment($id);
 
 		foreach ($expected_comment as $key => $value) {
 			if ($value === true) {
@@ -82,5 +84,47 @@ class CommentsTest extends PHPUnit_Framework_TestCase {
 				$this->assertEquals($value, $comment[$key]);
 			}
 		}
+	}
+
+	function testNewComment() {
+		$_SERVER = array(
+			'REMOTE_ADDR' => '0123abcdGHIJ:',
+			'HTTP_USER_AGENT' => str_repeat('abcdefghijklmnop', 17)
+		);
+
+		$id = wp_new_comment(array(
+			'comment_post_ID'   => 'post',
+			'comment_author'    => 'author',
+			'comment_author_email' => 'email',
+			'comment_author_url' => 'url',
+			'comment_author_IP' => 'ip',
+			'comment_date'      => 'date',
+			'comment_date_gmt'  => 'date-gmt',
+			'comment_content'   => 'content',
+			'comment_karma'     => 'karma',
+			'comment_approved'  => 'approved',
+			'comment_agent'     => 'agent',
+			'comment_type'      => 'type',
+			'comment_parent'    => 'parent',
+			'user_id'           => 'id',
+			'bad'               => 'bad',
+		));
+
+		$this->assertEquals(array(
+			'comment_post_ID'   => 0,
+			'comment_author'    => 'author',
+			'comment_author_email' => 'email',
+			'comment_author_url' => 'url',
+			'comment_author_IP' => '0123abcd:',
+			'comment_date'      => 'date',
+			'comment_date_gmt'  => 'date-gmt',
+			'comment_content'   => 'content',
+			'comment_karma'     => 'karma',
+			'comment_approved'  => 'approved',
+			'comment_agent'     => substr(str_repeat('abcdefghijklmnop', 16), 0, 254),
+			'comment_type'      => 'type',
+			'comment_parent'    => 'parent',
+			'user_id'           => 0,
+		), get_comment($id));
 	}
 }
