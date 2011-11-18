051fe8d8f3b6b19eee51ae9750a777f1770d024c
John Bintz <john@coswellproductions.com>
2011-11-18 08:46:09 -0500

    nuke all the things, point at new maintainer, good luck have fun

--- includes/filtering.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// stub functions for filtering. these do not filter!
-
-function esc_html($string) { return $string; }
-function esc_attr($string) { return $string; }
-function attribute_escape($string) { return $string; }
-function esc_js($string) { return $string; }
-function wp_filter_nohtml_kses($string) { return $string; }
